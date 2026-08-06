d66fd5bbf9f3b0755e8b69006027c4056370d325
Your afidhu <afidhumatata81@gmail.com>
2026-08-06 03:25:24 +0300

    fixed json

--- frontends/src/components/utils/api_provider/ApiProviders.tsx
+++ frontends/src/components/utils/api_provider/ApiProviders.tsx
@@ -3,7 +3,9 @@
 
 
 // class ApiProvider {
-  const  BaseUrl = 'http://localhost:51213'
+  // const  BaseUrl = 'http://localhost:51213'
+  const  BaseUrl = 'https://compliance-risk-and-safety-manageme-tau.vercel.app'
+  
   // const  BaseUrl = 'https://vd79567z-51213.inc1.devtunnels.ms'
 //     GetRisk =`${this.BaseUrl}/`https://vd79567z-51213.inc1.devtunnels.ms/
 // }
